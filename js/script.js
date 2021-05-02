ec4807a3fff0c7440323f8be222958f0ec21bf76
Amitsharma45 <sharma.amit20111@gmail.com>
2021-05-03 00:02:12 +0530

    fixed navbar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 function show(){
-    console.log("run");
+    // console.log("run");
+    var w = document.documentElement.clientWidth;
+    if(w<1128){
+
+        let visible = document.getElementById("h11");
+        // console.log(visible.style.display);
+        if(visible.style.display =='none'){
+            visible.style.display='block';
+        }else{
+            visible.style.display = 'none';
+        }
+    }
+}
+function s(){
+    var w = document.documentElement.clientWidth;
+    // console.log(w);
     let visible = document.getElementById("h11");
-    // console.log(visible.style.display);
-    if(visible.style.display =='none'){
+    if(w>1128){
         visible.style.display='block';
     }else{
         visible.style.display = 'none';
     }
+
 }
 
-console.log('portfolio');
+// console.log('portfolio');
