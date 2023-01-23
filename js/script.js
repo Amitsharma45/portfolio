e9be818dbca0fb0ead681420522858403e621228
Amitsharma45 <sharma.amit20111@gmaail.com>
2023-01-23 18:19:00 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".type",{
+    strings: ["Front End Developer","React Js Developer","MERN Stack Developer"],
+    typeSpeed:120,
+    backSpeed:75,
+    showCursor: true,
+    cursorChar: '',
+    loop:true
+
+})
