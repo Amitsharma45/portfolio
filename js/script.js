d441cba6d8968d4c5cee1373e51ece508a497a2d
Amitsharma45 <sharma.amit20111@gmaail.com>
2023-08-22 16:39:46 +0530

    completd

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".type",{
-    strings: ["Front End Developer","React Js Developer","MERN Stack Developer"],
+    strings: ["MERN Stack Developer","Front End Developer","React Js Developer"],
     typeSpeed:120,
     backSpeed:75,
     showCursor: true,
@@ -7,3 +7,22 @@ var typed = new Typed(".type",{
     loop:true
 
 })
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    // console.log(document.body.scrollTop)
+    console.log(document.documentElement.scrollTop)
+    if (document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
