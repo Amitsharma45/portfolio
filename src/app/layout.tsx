46772a7470471bfd643ce62f8eea6d0d3750aab7
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-30 22:29:12 +0530

    initialized bricolage-grotesque font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from "next";
 import { Theme } from "@radix-ui/themes";
+import { Bricolage_Grotesque } from "next/font/google";
 import '@radix-ui/themes/styles.css';
 import './globals.css'
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+
+const bricolage_grotesque_init = Bricolage_Grotesque({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bricolage-grotesque",
+});
 
 export const metadata: Metadata = {
   title: "Fardeen Mansoori",
@@ -17,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={bricolage_grotesque_init.variable}>
         <Theme appearance='dark' >
           <Navbar />
           {children}
