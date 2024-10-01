5d274ff4ff708848d6bfeba17d991023ff8941d2
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-01 18:28:16 +0530

    added blog page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center bricolage-grotesque text-xl'>Coming Soon...</div>
+  )
+}
+
+export default page
