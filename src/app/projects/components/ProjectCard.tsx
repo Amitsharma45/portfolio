6113ee0a41478ff1efd534cfde4d059d4087e333
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-01 14:00:10 +0530

    changes in experience and project card

--- src/app/projects/components/ProjectCard.tsx
+++ src/app/projects/components/ProjectCard.tsx
@@ -8,8 +8,8 @@ import React from 'react'
 
 const ProjectCard = ({logo,title, description, techStack, link, source } : Project) => {
   return (
-        <MagicCard className="cursor-pointer shadow-2xl w-[45%] max-sm:!w-fit h-[300px]" gradientColor= "#262626">
-            <ShineBorder className="border h-full w-full relative rounded-lg flex flex-col justify-center items-start md:shadow-xl !bg-transparent !pointer-events-none" color={["#FF9933", "#FFFFFF", "#138808"]}>
+        <MagicCard className="cursor-pointer shadow-2xl w-[45%] max-sm:!w-fit h-[300px] border-none !bg-transparent" gradientColor= "#262626">
+            <ShineBorder className="border bg-transparent h-full w-full relative rounded-lg flex flex-col justify-center items-start md:shadow-xl !bg-transparent !pointer-events-none" color={["#FF9933", "#FFFFFF", "#138808"]}>
                 
                 <div className='px-3'>
                     <Image src={logo} alt='project-logo' width={30} height={30} />
@@ -18,11 +18,11 @@ const ProjectCard = ({logo,title, description, techStack, link, source } : Proje
                     <h1 className='text-xl font-bold tracking-tight text-start bricolage-grotesque'>{title}</h1>
                     <p className='mt-2 text-sm text-gray-400 bricolage-grotesque'>{description}</p>
                 </div>
-                <div className="flex gap-1 px-3 mt-4 flex-wrap">
+                <div className="flex gap-1 px-3 mt-4 flex-wrap !pointer-events-auto">
                     {
                         techStack.map((tech, idx) => (
-                            <Badge key={idx} color="gray" variant="outline" highContrast className='text-[10px] bricolage-grotesque'>
-                                {tech}                     
+                            <Badge key={idx} color="gray" variant="outline" highContrast className='text-[10px] bricolage-grotesque hover:!bg-white hover:!text-black !pointer-events-auto'>
+                                {tech}    
                             </Badge>
                         ))
                     }
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -11,24 +11,24 @@ const Experience = () => {
               </h1>
         </div>
 
-<MagicCard className="cursor-pointer shadow-2xl mt-5 !bg-transparent !border-none" gradientColor= "#262626">
-        <div className="flex w-full px-5 py-3 bricolage-grotesque">
-          <div className="w-24 flex items-center justify-center">
-              <Image src='/cmp.jpeg' alt='company-logo' width={50} height={50} className='rounded-full'/>
-          </div>
-          <div className="w-full">
-            <div className="">
-              <div className="flex justify-between">
-                <h1 className='text-lg font-semibold'>Apna College</h1>
-                <span className='text-xs'>Feb 2024 - June 2024</span>
+        <MagicCard className="cursor-pointer shadow-2xl mt-5 !bg-transparent border-none" gradientColor= "#262626">
+          <div className="flex w-full px-5 py-3 bricolage-grotesque">
+            <div className="w-24 flex items-center justify-center">
+                <Image src='/cmp.jpeg' alt='company-logo' width={50} height={50} className='rounded-full'/>
+            </div>
+            <div className="w-full">
+              <div className="">
+                <div className="flex justify-between">
+                  <h1 className='text-lg font-semibold'>Apna College</h1>
+                  <span className='text-xs'>Feb 2024 - June 2024</span>
+                </div>
+                <h2 className='text-sm'>Teaching Assistant (MERN STACK)</h2>
               </div>
-              <h2 className='text-sm'>Teaching Assistant (MERN STACK)</h2>
-            </div>
-            <div className="mt-3 text-sm">
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto maxime harum facere voluptas officiis doloribus nobis accusamus, laboriosam ratione in!</p>
+              <div className="mt-3 text-sm">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto maxime harum facere voluptas officiis doloribus nobis accusamus, laboriosam ratione in!</p>
+              </div>
             </div>
           </div>
-        </div>
         </MagicCard>
     </div>
   )
