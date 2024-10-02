ffa1a75ff8174825fd94b5010828d67f11e15304
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-02 15:32:39 +0530

    added hover effacts on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,45 +10,45 @@ const Footer = () => {
         <Flex gap='5' justify='center' align='center' px='2' direction='column' >
             <div>
                 <Tooltip content="Resume">
-                    <Link href="https://drive.google.com/file/d/1BvmKeNsKERxnUUIRJcGf-grLydwr-yfn/view?usp=sharing" target='blank' className='text-black dark:text-white hover:text-purple-300'>
-                        <FileIcon width={20} height={20} />
+                    <Link href="https://drive.google.com/file/d/1BvmKeNsKERxnUUIRJcGf-grLydwr-yfn/view?usp=sharing" target='blank' underline='none'>
+                        <FileIcon width={20} height={20} className='text-black dark:text-white hover:scale-110'/>
                     </Link>
                 </Tooltip>
             </div>
 
             <div>
                 <Tooltip content="Blog">
-                    <Link href="/blogs" className='text-black dark:text-white hover:text-purple-300'>
-                        <RowsIcon width={17} height={17} />
+                    <Link href="/blogs">
+                        <RowsIcon width={17} height={17} className='text-black dark:text-white hover:scale-110'/>
                     </Link>
                 </Tooltip>
             </div>
             <Separator orientation='horizontal' size='1' className='bg-black dark:bg-gray-400' />
             <div>
                 <Tooltip content="Github">
-                    <Link href="https://github.com/Fardeen26" target='blank' className='text-black dark:text-white hover:text-purple-300'>
-                        <GitHubLogoIcon width={20} height={20} />
+                    <Link href="https://github.com/Fardeen26" target='blank'>
+                        <GitHubLogoIcon width={20} height={20} className='text-black dark:text-white hover:scale-110' />
                     </Link>
                 </Tooltip>
             </div>
             <div>
                 <Tooltip content="x (twitter)">
-                    <Link href="https://twitter.com/fardeen14693425" target='blank' className='text-black dark:text-white hover:text-purple-400'>
-                        <TwitterLogoIcon width={20} height={20} />
+                    <Link href="https://twitter.com/fardeen14693425" target='blank'>
+                        <TwitterLogoIcon width={20} height={20} className='text-black dark:text-white hover:scale-110' />
                     </Link>
                 </Tooltip>
             </div>
             <div>
                 <Tooltip content="Linkedin">
-                    <Link href="https://www.linkedin.com/in/fardeenmansoori/" target='blank' className='text-black dark:text-white hover:text-purple-300'>
-                        <LinkedInLogoIcon width={20} height={20} />
+                    <Link href="https://www.linkedin.com/in/fardeenmansoori/" target='blank'>
+                        <LinkedInLogoIcon width={20} height={20} className='text-black dark:text-white hover:scale-110' />
                     </Link>
                 </Tooltip>
             </div>
             <div>
                 <Tooltip content="Geeks for Geeks">
-                    <Link href="https://www.geeksforgeeks.org/user/fardeenmanxtyn/" target='blank' className='text-black dark:text-white hover:text-purple-300'>
-                        <CodeIcon width={20} height={20} />
+                    <Link href="https://www.geeksforgeeks.org/user/fardeenmanxtyn/" target='blank'>
+                        <CodeIcon width={20} height={20} className='text-black dark:text-white hover:scale-110' />
                     </Link>
                 </Tooltip>
             </div>
