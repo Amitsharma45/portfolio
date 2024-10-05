f818093904923c847c2b4532b287b70a0e66c5f2
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-05 19:16:20 +0530

    created see more and see less functionality

--- src/app/about/components/About.tsx
+++ src/app/about/components/About.tsx
@@ -1,18 +1,20 @@
+'use client';
 import Heading from '@/components/ui/heading'
 import { I_About } from '@/types/project'
 import { bricolage_grotesque } from '@/utils/fonts'
 import { Separator } from '@radix-ui/themes'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const About = () => {
+    const [isMore, setIsMore] = useState<boolean>(false);
   return (
     <div className='w-full px-64 max-sm:px-2 flex flex-col items-center mt-4 pb-8'>
         <Heading title='About My Journey'/>
 
         <div className="w-full pl-36 pr-28 max-sm:px-2">
 
-            {data.map((item, idx) => (
+            {data.slice(0, 3).map((item, idx) => (
                 <>
                 <h1 className={`text-2xl mt-8 font-medium ${bricolage_grotesque}`}>{item.year}</h1>
                 <div key={idx} className="flex pl-2 mt-4">               
@@ -29,7 +31,30 @@ const About = () => {
             </div>
             </>
             ))}
+
+            <div className={`${isMore ? 'hidden' : 'block'}`} onClick={() => setIsMore(true)}>Read More</div>
+
+            {
+                isMore && data.slice(3).map((item, idx) => (
+                    <>
+                <h1 className={`text-2xl mt-8 font-medium ${bricolage_grotesque}`}>{item.year}</h1>
+                <div key={idx} className="flex pl-2 mt-4">               
+                <div className={`w-full flex flex-col gap-3 ${bricolage_grotesque}`}>
+
+                    {item.events.map((incd, idx) => (
+                        <div key={idx} className={`flex items-center gap-2 ${bricolage_grotesque}`}>
+                            <span><Separator orientation='horizontal' size='1' className='w-8 bg-black dark:bg-gray-400'/></span>
+                            <span className='text-[15px] dark:text-[#dfdede]'>{incd}</span>
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+            </>
+                ))
+            }
             
+            <button className={`${isMore ? 'block' : 'hidden'}`} onClick={() => setIsMore(false)}>See less</button>
         </div>
     </div>
   )
@@ -62,5 +87,53 @@ const data : I_About[] = [
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem3.",
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem3.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem2.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem2.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem2."
         ]
-    }
+    },
+    {
+        year: 2021,
+        events: [
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem."
+        ]
+    },
+    {
+        year: 2020,
+        events: [
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem."
+        ]
+    },
+    {
+        year: 2019,
+        events: [
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem."
+        ]
+    },
+    {
+        year: 2017,
+        events: [
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem."
+        ]
+    },
+    {
+        year: 2015,
+        events: [
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem."
+        ]
+    },
+    {
+        year: 2005,
+        events: [
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem.",
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, exercitationem."
+        ]
+    },
 ]
