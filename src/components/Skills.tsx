e7592340b16c6b9cfc3183f9efb27831c1f4f42a
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-20 22:24:53 +0530

    feat: added some more skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Badge, Tooltip } from '@radix-ui/themes'
 import React from 'react'
 import { bricolage_grotesque } from '@/utils/fonts'
@@ -6,24 +8,26 @@ import Title from './ui/Title'
 const Skills = () => {
   return (
     <div className='w-2/3 max-lg:w-full max-sm:w-full flex flex-col items-center mt-4 pb-8'>
-          <Title title='Skills' />
+      <Title title='Skills' />
 
-          <div className="flex w-full flex-col max-lg:flex-row max-sm:flex-row gap-3 max-sm:gap-2 lg:flex-row mt-4 px-40 max-[1285px]:px-24 max-lg:px-28 max-sm:px-6 flex-wrap justify-center items-center max-sm:ml-">
-            {
-                data.map((skill, idx) => (   
-                <Tooltip key={idx} content={skill}>
-                    <Badge color="gray" variant="solid" highContrast className={`text-xs max-sm:text-[10px] dark:hover:bg-gray-300 py-1 px-2 cursor-pointer hover:bg-gray-800 ${bricolage_grotesque}`}>
-                        {skill}
-                    </Badge>
-                </Tooltip>
-                ))
-            }
-          </div>
+      <div className="flex w-full flex-col max-lg:flex-row max-sm:flex-row gap-3 max-sm:gap-2 lg:flex-row mt-4 px-40 max-[1285px]:px-24 max-lg:px-28 max-sm:px-6 flex-wrap justify-center items-center max-sm:ml-">
+        {
+          data.map((skill, idx) => (
+            <Tooltip key={idx} content={skill}>
+              <Badge color="gray" variant="solid" highContrast className={`text-xs max-sm:text-[10px] dark:hover:bg-gray-300 py-1 px-2 cursor-pointer hover:bg-gray-800 ${bricolage_grotesque}`}>
+                {skill}
+              </Badge>
+            </Tooltip>
+          ))
+        }
+      </div>
     </div>
   )
 }
 
 
-export default Skills
+export default Skills;
 
-const data: string[] = ["NextJS","TypeScript","React","Tailwind CSS","NodeJS","Express","MongoDB","MySQL","REST APIs","GitHub","Vercel","VS Code","Bootstrap","Solidity","C","C++","Java","Redux","foundry","Web3.js","Ethers.js","Zod","MUI","Shadcn","Remix", ];
+const data: string[] = ["JavaScript", "TypeScript", "C++", "Java", "Solidity", "NextJS", "React", "Prisma", "MongoDB", "PostgreSql", "MySQL", "Docker", "Redux", "Recoil", "Tailwind CSS", "Bootstrap", "NodeJS", "ExpressJS", "foundry", "Zod", "Git"];
+
+// const data: string[] = ["NextJS", "TypeScript", "React", "Tailwind CSS", "NodeJS", "Express", "MongoDB", "MySQL", "REST APIs", "GitHub", "Vercel", "VS Code", "Bootstrap", "Solidity", "C", "C++", "Java", "Redux", "foundry", "Web3.js", "Ethers.js", "Zod", "MUI", "Shadcn", "Remix",];
