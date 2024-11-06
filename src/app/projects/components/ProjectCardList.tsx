e997c19b6fc822702e2e844c48b85c82511fb231
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-06 19:43:42 +0530

    feat: added special component for an project

--- src/app/projects/components/ProjectCardList.tsx
+++ src/app/projects/components/ProjectCardList.tsx
@@ -2,25 +2,46 @@ import { Project } from '@/types/project';
 import React from 'react'
 import ProjectCard from './ProjectCard';
 import Title from '@/components/ui/Title';
+import { MagicCard } from '@/components/ui/magic-card';
+import ShineBorder from '@/components/ui/shine-border';
+import { Link } from '@radix-ui/themes';
+import Image from 'next/image';
+import { bricolage_grotesque, inter } from '@/utils/fonts';
 
 
 const ProjectCardList = () => {
   return (
     <div className='w-full px-64 max-[1025px]:px-4 max-[1285px]:px-40 max-lg:px-0 max-sm:px-4 flex flex-col items-center mt-4 pb-8'>
-          <Title title='Projects'/>
-          <div className="flex w-full flex-col gap-4 lg:flex-row mt-4 px-32 max-lg:px-0 max-sm:px-2 flex-wrap items-center ml-14 max-sm:ml-0 max-lg:ml-0 max-[350px]:mr-5 max-[321px]:mr-10">
-              { data.map((project: Project, idx: number) => (
-                  <ProjectCard 
-                    key={idx} 
-                    logo={project.logo} 
-                    title={project.title} 
-                    description={project.description} 
-                    techStack={project.techStack} 
-                    link={project.link} 
-                    source={project.source} 
-                  />
-              ))}
+      <Title title='Projects' />
+
+      <MagicCard gradientColor={'#262626'} className='w-[45vw] flex justify-center !bg-transparent cursor-pointer mt-5'>
+        <ShineBorder color={["#FF9933", "#FFFFFF", "#138808"]} className='w-[45vw] !bg-transparent !pointer-events-none flex p-5'>
+          <div className="w-[15vw]">
+            <Link href='#' target='_blank'>
+              <Image src='/uok.gif' alt='company-logo' width={50} height={50} className='rounded-full' />
+            </Link>
           </div>
+          <div className="">
+            <h1 className={`text-xl text-black dark:text-white font-bold tracking-tight text-start whitespace-nowrap ${bricolage_grotesque}`}>Textify - Adobe Funded Project</h1>
+            <p className={`mt-2 text-sm dark:text-[#D1D5DB] ${inter}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </div>
+        </ShineBorder>
+      </MagicCard>
+
+      <div className="flex w-full flex-col gap-4 lg:flex-row mt-4 px-32 max-lg:px-0 max-sm:px-2 flex-wrap items-center ml-14 max-sm:ml-0 max-lg:ml-0 max-[350px]:mr-5 max-[321px]:mr-10">
+        {data.map((project: Project, idx: number) => (
+          <ProjectCard
+            key={idx}
+            logo={project.logo}
+            title={project.title}
+            description={project.description}
+            techStack={project.techStack}
+            link={project.link}
+            source={project.source}
+          />
+        ))}
+      </div>
+
     </div>
   )
 }
@@ -28,55 +49,55 @@ const ProjectCardList = () => {
 export default ProjectCardList;
 
 const data: Project[] = [
-    {
-      logo: '/enrich.png',
-      title: "Enrich Salon Management",
-      description: "Enrich Hair Salon is a salon booking system with online payments and an admin dashboard for managing services and bookings.",
-      techStack: ["React", "MUI", "Tailwind CSS", "Razorpay", "Nodemailer", "React Charts", "NodeJS", "MongoDB"],
-      link: "https://enrich-hair-salon.vercel.app/",
-      source: "https://github.com/Fardeen26/enrich-salon-management",
-    },
-    {
-      logo: '/orbittoken.png',
-      title: "OrbitToken",
-      description: "OrbitToken enables users launch their custom tokens on the Solana blockchain, with features to transfer tokens, view token balances, transfer SOL, receive airdrops, and check account balances.",
-      techStack: ["React", "Web3.js", "spl-token", "TailwindCSS"],
-      link: "https://orbit-token-chi.vercel.app/",
-      source: "https://github.com/Fardeen26/OrbitToken",
-    },
-    {
-      logo: '/gobegin.webp',
-      title: "GoBegin booking application",
-      description: "GoBegin is a platform where users can list residences, view details, comment, and see locations on a map—similar to Airbnb.",
-      techStack: ["NodeJS", "ExpressJS", "MongoDB", "Bootstrap", "PassportJS", "EJS"],
-      link: "https://wanderlust-6cr5.onrender.com/",
-      source: "https://github.com/Fardeen26/wanderlust_app",
-    },
-    
-    {
-      logo: "/lexicon.jpeg",
-      title: "Lexicon",
-      description: "Lexicon is a platform for discovering, downloading, and uploading books with a clean, user-friendly interface, working on auth and review functionality.",
-      techStack: ["NextJS", "Typescript", "Tailwind CSS", "MongoDB", "Zod"],
-      link: "https://lexicon-sand.vercel.app/",
-      source: "https://github.com/Fardeen26/lexicon",
-    },
-   
-    {
-      logo: "/sumz.svg",
-      title: "SumZ - Article Summarizer",
-      description: "SumZ is an article summarizer that quickly generates concise summaries of lengthy articles, helping users grasp key points efficiently. It's designed for speed and accuracy with a clean, user-friendly interface.",
-      techStack: ["React", "OpenAI", "Tailwind CSS"],
-      link: "https://sumz-summarize.netlify.app/",
-      source: "https://github.com/Fardeen26/Article-Summarizer",
-    },
+  {
+    logo: '/enrich.png',
+    title: "Enrich Salon Management",
+    description: "Enrich Hair Salon is a salon booking system with online payments and an admin dashboard for managing services and bookings.",
+    techStack: ["React", "MUI", "Tailwind CSS", "Razorpay", "Nodemailer", "React Charts", "NodeJS", "MongoDB"],
+    link: "https://enrich-hair-salon.vercel.app/",
+    source: "https://github.com/Fardeen26/enrich-salon-management",
+  },
+  {
+    logo: '/orbittoken.png',
+    title: "OrbitToken",
+    description: "OrbitToken enables users launch their custom tokens on the Solana blockchain, with features to transfer tokens, view token balances, transfer SOL, receive airdrops, and check account balances.",
+    techStack: ["React", "Web3.js", "spl-token", "TailwindCSS"],
+    link: "https://orbit-token-chi.vercel.app/",
+    source: "https://github.com/Fardeen26/OrbitToken",
+  },
+  {
+    logo: '/gobegin.webp',
+    title: "GoBegin booking application",
+    description: "GoBegin is a platform where users can list residences, view details, comment, and see locations on a map—similar to Airbnb.",
+    techStack: ["NodeJS", "ExpressJS", "MongoDB", "Bootstrap", "PassportJS", "EJS"],
+    link: "https://wanderlust-6cr5.onrender.com/",
+    source: "https://github.com/Fardeen26/wanderlust_app",
+  },
 
-    {
-      logo: "/ghostgram.png",
-      title: "GhostGram - SaaS platform",
-      description: "GhostGram is an anonymous messaging platform that lets users securely send and receive messages through unique, authenticated links.",
-      techStack: ["NextJS", "Typescript", "MongoDB", "NextAuth", "TailwindCSS", "Shadcn"],
-      link: "",
-      source: "https://github.com/Fardeen26/ghostgram",
-    },
-  ];
+  {
+    logo: "/lexicon.jpeg",
+    title: "Lexicon",
+    description: "Lexicon is a platform for discovering, downloading, and uploading books with a clean, user-friendly interface, working on auth and review functionality.",
+    techStack: ["NextJS", "Typescript", "Tailwind CSS", "MongoDB", "Zod"],
+    link: "https://lexicon-sand.vercel.app/",
+    source: "https://github.com/Fardeen26/lexicon",
+  },
+
+  {
+    logo: "/sumz.svg",
+    title: "SumZ - Article Summarizer",
+    description: "SumZ is an article summarizer that quickly generates concise summaries of lengthy articles, helping users grasp key points efficiently. It's designed for speed and accuracy with a clean, user-friendly interface.",
+    techStack: ["React", "OpenAI", "Tailwind CSS"],
+    link: "https://sumz-summarize.netlify.app/",
+    source: "https://github.com/Fardeen26/Article-Summarizer",
+  },
+
+  {
+    logo: "/ghostgram.png",
+    title: "GhostGram - SaaS platform",
+    description: "GhostGram is an anonymous messaging platform that lets users securely send and receive messages through unique, authenticated links.",
+    techStack: ["NextJS", "Typescript", "MongoDB", "NextAuth", "TailwindCSS", "Shadcn"],
+    link: "",
+    source: "https://github.com/Fardeen26/ghostgram",
+  },
+];
