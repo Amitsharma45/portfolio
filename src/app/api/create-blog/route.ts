cd6cb6866076cc2f2e0d8991f4ca4fc7185d724d
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-10 15:04:34 +0530

    fix: added server side authentication

--- src/app/api/create-blog/route.ts
+++ src/app/api/create-blog/route.ts
@@ -17,7 +17,6 @@ interface CloudinaryUploadResult {
 export async function POST(req: NextRequest) {
     const session = await getServerSession();
 
-    console.log(session)
     if (!session) {
         return NextResponse.json(
             { success: false, message: "You are Unauthorized!" },
@@ -31,54 +30,54 @@ export async function POST(req: NextRequest) {
         const title = formData.get("title");
         const content = formData.get("content");
 
-        // if (!file) {
-        //     return NextResponse.json(
-        //         { success: false, error: "File not found!" },
-        //         { status: 400 }
-        //     )
-        // }
+        if (!file) {
+            return NextResponse.json(
+                { success: false, error: "File not found!" },
+                { status: 400 }
+            )
+        }
 
-        // if (!title) {
-        //     return NextResponse.json(
-        //         { success: false, error: "Title not found!" },
-        //         { status: 400 }
-        //     )
-        // }
+        if (!title) {
+            return NextResponse.json(
+                { success: false, error: "Title not found!" },
+                { status: 400 }
+            )
+        }
 
-        // if (!content) {
-        //     return NextResponse.json(
-        //         { success: false, error: "Content not found!" },
-        //         { status: 400 }
-        //     )
-        // }
-
-
-        // const bytes = await file.arrayBuffer()
-        // const buffer = Buffer.from(bytes)
-
-        // const result = await new Promise<CloudinaryUploadResult>(
-        //     (resolve, reject) => {
-        //         const uploadStream = cloudinary.uploader.upload_stream(
-        //             { folder: "next-cloudinary-uploads" },
-        //             (error, result) => {
-        //                 if (error) reject(error);
-        //                 else resolve(result as CloudinaryUploadResult);
-        //             }
-        //         )
-        //         uploadStream.end(buffer)
-        //     }
-        // )
+        if (!content) {
+            return NextResponse.json(
+                { success: false, error: "Content not found!" },
+                { status: 400 }
+            )
+        }
 
 
-        // await prisma.blog.create({
-        //     data: {
-        //         title: title.toString(),
-        //         content: content.toString(),
-        //         author: session?.user?.name as string,
-        //         image_public_id: result.public_id,
-        //         createdAt: new Date()
-        //     }
-        // })
+        const bytes = await file.arrayBuffer()
+        const buffer = Buffer.from(bytes)
+
+        const result = await new Promise<CloudinaryUploadResult>(
+            (resolve, reject) => {
+                const uploadStream = cloudinary.uploader.upload_stream(
+                    { folder: "next-cloudinary-uploads" },
+                    (error, result) => {
+                        if (error) reject(error);
+                        else resolve(result as CloudinaryUploadResult);
+                    }
+                )
+                uploadStream.end(buffer)
+            }
+        )
+
+
+        await prisma.blog.create({
+            data: {
+                title: title.toString(),
+                content: content.toString(),
+                author: session?.user?.name as string,
+                image_public_id: result.public_id,
+                createdAt: new Date()
+            }
+        })
 
         return NextResponse.json(
             { success: true, message: "Blog published successfully!" },
