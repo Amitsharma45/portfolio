7bb561c3cf263f497438268e97877d38f99682f7
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-10 10:21:47 +0530

    fix: type error

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { SessionProvider } from "next-auth/react"
+import { ReactNode } from "react"
 
-export default function Provider({ children }) {
+export default function Provider({ children }: { children: ReactNode }) {
     return (
         <SessionProvider>
             {children}
