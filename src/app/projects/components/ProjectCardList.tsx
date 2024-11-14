f7f05a8eb5b5b67b407d5e010c8b445213ac90d4
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-14 20:34:55 +0530

    feat: added portfolio project card

--- src/app/projects/components/ProjectCardList.tsx
+++ src/app/projects/components/ProjectCardList.tsx
@@ -1,43 +1,17 @@
 'use client'
 
 import { Project } from '@/types/project';
-import React, { useContext } from 'react'
 import ProjectCard from './ProjectCard';
 import Title from '@/components/ui/Title';
-import { MagicCard } from '@/components/ui/magic-card';
-import ShineBorder from '@/components/ui/shine-border';
-import { Badge, Link } from '@radix-ui/themes';
-import Image from 'next/image';
-import { bricolage_grotesque, inter } from '@/utils/fonts';
-import { DarkModeContext } from '@/context/DarkModeContext';
-import { GitHubLogoIcon, GlobeIcon } from '@radix-ui/react-icons';
+import SimpleProjectCard from '@/components/SimpleProjectCard';
+import MaxProjectCard from '@/components/MaxProjectCard';
 
 
 const ProjectCardList = () => {
-  const darkModeContext = useContext(DarkModeContext);
-
-  if (!darkModeContext) {
-    throw new Error("Navbar must be used within a DarkModeProvider");
-  }
-
-  const { isDarkMode } = darkModeContext;
   return (
     <div className='w-full h-fit px-64 max-[1025px]:px-4 max-[1285px]:px-40 max-lg:px-0 max-sm:px-4 flex flex-col items-center mt-4 pb-8'>
       <Title title='Projects' />
-
-      <MagicCard gradientColor={`${isDarkMode ? '#262626' : 'rgba(197, 241, 241, 0.4)'}`} className='w-[68%] max-lg:w-[45%] max-sm:w-[95%] flex justify-center max-lg:justify-start !bg-transparent cursor-pointer mt-5 rounded-lg dark:shadow-2xl'>
-        <ShineBorder color={["#FF9933", "#FFFFFF", "#138808"]} className='w-full !bg-transparent !pointer-events-none flex max-lg:flex-col max-lg:items-start p-5 md:shadow-xl'>
-          <div className="w-[10vw] max-[1285px]:w-[13vw]">
-            <Link href='#' target='_blank'>
-              <Image src='/adobe.png' alt='company-logo' width={50} height={50} className='rounded-full max-lg:h-10 max-lg:w-10' />
-            </Link>
-          </div>
-          <div className="max-lg:mt-3">
-            <h1 className={`text-xl max-lg:text-lg text-black dark:text-white font-bold tracking-tight text-start ${bricolage_grotesque}`}>Textify: Adobe-Funded Add-On for Adobe Express</h1>
-            <p className={`mt-2 text-sm dark:text-[#D1D5DB] ${inter}`}>Textify is an Adobe-funded add-on that enables seamless text extraction from images in Adobe Express, supporting formats like PNG, JPEG, and SVG to enhance creative workflows.</p>
-          </div>
-        </ShineBorder>
-      </MagicCard>
+      <SimpleProjectCard />
 
       <div className="flex w-full flex-col gap-4 lg:flex-row mt-4 px-32 max-lg:px-0 max-sm:px-2 flex-wrap items-center ml-14 max-sm:ml-0 max-lg:ml-0 max-[350px]:mr-5 max-[321px]:mr-10">
         {data.map((project: Project, idx: number) => (
@@ -51,8 +25,8 @@ const ProjectCardList = () => {
             source={project.source}
           />
         ))}
-
       </div>
+      <MaxProjectCard />
     </div>
   )
 }
---
+++ src/components/MaxProjectCard.tsx
@@ -0,0 +1,86 @@
+import React, { useContext } from 'react'
+import { MagicCard } from './ui/magic-card'
+import ShineBorder from './ui/shine-border';
+import { bricolage_grotesque, inter } from '@/utils/fonts';
+import { Badge, Link } from '@radix-ui/themes';
+import Image from 'next/image';
+import { DarkModeContext } from '@/context/DarkModeContext';
+import { GitHubLogoIcon, GlobeIcon } from '@radix-ui/react-icons';
+import { Project } from '@/types/project';
+
+const MaxProjectCard = () => {
+    const darkModeContext = useContext(DarkModeContext);
+
+    if (!darkModeContext) {
+        throw new Error("Navbar must be used within a DarkModeProvider");
+    }
+
+    const { isDarkMode } = darkModeContext;
+    return (
+        <>
+            {
+                data.map((project, idx) => (
+                    <MagicCard key={idx} gradientColor={`${isDarkMode ? '#262626' : 'rgba(197, 241, 241, 0.4)'}`} className='w-[68%] h-fit max-lg:w-[45%] max-sm:w-[95%] flex justify-center max-lg:justify-start !bg-transparent cursor-pointer mt-5 rounded-lg dark:shadow-2xl'>
+                        <ShineBorder color={["#FF9933", "#FFFFFF", "#138808"]} className='w-[44.5vw] max-[1285px]:w-full max-sm:w-full h-full !bg-transparent !pointer-events-none flex items-center max-lg:flex-col max-lg:items-start pl-2 pr-5 py-5 max-sm:py-5 md:shadow-xl space-x-4'>
+
+                            <div className="w-[5vw] max-[1285px]:w-[13vw]">
+                                <Link href={project.link} target='_blank'>
+                                    <Image src='/portfolio-logo.jpg' alt='company-logo' width={55} height={55} className='rounded-full border border-black max-lg:h-10 max-lg:w-10' />
+                                </Link>
+                            </div>
+                            <div>
+                                <div className="max-lg:mt-3">
+                                    <Link href={project.link ? project.link : project.source} target='_blank' underline='none'>
+                                        <h1 className={`text-xl max-lg:text-lg text-black dark:text-white font-bold tracking-tight text-start ${bricolage_grotesque}`}>{project.title}</h1>
+                                    </Link>
+                                    <p className={`mt-1 text-sm dark:text-[#D1D5DB] ${inter}`}>{project.description}</p>
+                                </div>
+
+                                <div className="flex gap-1 mt-3 flex-wrap !pointer-events-auto">
+                                    {
+                                        project.techStack?.map((tech, idx) => (
+                                            <Badge key={idx} color="gray" variant="outline" highContrast className={`text-[10px] dark:hover:!bg-white hover:!bg-black hover:!text-white dark:hover:!text-black !pointer-events-auto ${bricolage_grotesque}`}>
+                                                {tech}
+                                            </Badge>
+                                        ))
+                                    }
+                                </div>
+                                <div className='mt-3 !pointer-events-auto flex gap-1'>
+                                    {
+                                        project.link && (
+                                            <Link href={project.link} target='_blank'>
+                                                <Badge color="gray" variant="solid" highContrast className={`text-[10px] py-[3px] dark:hover:bg-gray-300 ${bricolage_grotesque}`}>
+                                                    <GlobeIcon width={11} height={11} /> Website
+                                                </Badge>
+                                            </Link>
+                                        )
+                                    }
+
+                                    <Link href={project.source} target='_blank'>
+                                        <Badge color="gray" variant="solid" highContrast className={`text-[10px] py-[3px] dark:hover:bg-gray-300 ${bricolage_grotesque}`}>
+                                            <GitHubLogoIcon width={11} height={11} /> Source
+                                        </Badge>
+                                    </Link>
+                                </div>
+                            </div>
+                        </ShineBorder>
+                    </MagicCard>
+                ))
+            }
+
+        </>
+    )
+}
+
+export default MaxProjectCard;
+
+const data: Project[] = [
+    {
+        logo: '/portfolio-logo.jpg',
+        title: "Portfolio",
+        description: "A portfolio crafted and developed by me, showcasing my skills, projects, and passion for web development in a sleek, user-friendly design.",
+        techStack: ["NextJS", "TypeScript", "Prisma", "PostgreSQL", "NextAuth", "TipTap Editor", "Resend", "Cloudinary", "Zod", "framer-motion", "Radix UI", "Magic UI", "Tailwind CSS"],
+        link: "https://fardeenmansoori.vercel.app/",
+        source: "https://github.com/Fardeen26/portfolio",
+    }
+];
