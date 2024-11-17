d348cb6998ca085e7ef8942045a7bb4581186ffd
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-17 12:16:11 +0530

    fix: footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className='w-full px-[400px] max-[1285px]:px-[350px] max-lg:px-64 max-sm:px-4 py-10 flex flex-col max-sm:text-center items-center mt-4 pb-8'>
       <div className="w-full">
-        <p className='text-sm max-sm:text-xs'>Crafted and Created by <Link href='https://x.com/intent/follow?screen_name=fardeen14693425' target='_blank' >Fardeen Mansoori</Link></p>
+        <p className='text-sm max-sm:text-xs'>Designed and Developed by <Link href='https://x.com/intent/follow?screen_name=fardeen14693425' target='_blank' >Fardeen Mansoori</Link></p>
       </div>
 
     </footer>
